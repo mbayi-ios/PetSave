72345087dfa0f5811fab495c85ec6e24b3f9a288
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-12-09 17:40:03 +0300

    - add unit tests
    - add unit tests

---
+++ PetSaveTests/Tests/Core/data/api/mock/APIManagerMock.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import PetSave
+
+struct APIManagerMock: APIManagerProtocol {
+  func perform(_ request: RequestProtocol, authToken: String) async throws -> Data {
+    return try Data(contentsOf: URL(fileURLWithPath: request.path), options: .mappedIfSafe)
+
+  }
+
+  func requestToken() async throws -> Data {
+    Data(AccessTokenTestHelper.generateValidToken().utf8)
+  }
+}
---
+++ PetSaveTests/Tests/Core/data/api/mock/AnimalsRequestMock.swift
@@ -0,0 +1,16 @@
+import Foundation
+@testable import PetSave
+
+enum AnimalsRequestMock: RequestProtocol {
+  case getAnimals
+
+  var requestType: RequestType {
+    return .GET
+  }
+
+  var path: String {
+    guard let path = Bundle.main.path(forResource: "AnimalsMock", ofType: "json")
+    else { return ""}
+    return path
+  }
+}
